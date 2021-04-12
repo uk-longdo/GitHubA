009877c1172bfbbdc2bdc82e17e0a4847dd2629a
uk-longdo <alsdnr729@gmail.com>
2021-04-12 16:38:56 +0900

    for문 if문

--- src/TestP/testCode.java
+++ src/TestP/testCode.java
@@ -1,8 +1,35 @@
 package TestP;
 
 public class testCode {
+	
+	public static void sum() {
+		int s = 0;
+		for(int i=1; i<1000; i++) {
+			if(i%3==0) {
+				s +=i;
+			}
+			
+		}
+		System.out.println("3의 배수 합 ="+s);
+		
+	}
+	public static void sum2() {
+		int v = 0;
+		for(int i=1; i<1000; i++) {
+			if(i%5==0) {
+				v +=i;
+			}
+		}
+		System.out.println("5의배수 합 =" +v);
+	}
+	
+	
 	public static void main(String[] args) {
-		System.out.println("Hellow word");
+		sum();
+		sum2();
 		//git test
 	}
+	
+	
+	
 }
